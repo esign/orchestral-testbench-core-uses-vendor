6a601f49b444793c42f5ee8803b66ab301a14355
jordyvanderhaegen <jordy.vanderhaegen@gmail.com>
2025-12-03 09:04:44 +0100

    Initial commit

---
+++ tests/TemplateTest.php
@@ -0,0 +1,15 @@
+<?php
+
+
+use PHPUnit\Framework\Attributes\Test;
+
+namespace :vendor_namespace\Tests;
+
+final class :studly_package_nameTest extends TestCase
+{
+    #[Test]
+    public function test(): void
+    {
+        $this->assertTrue(true);
+    }
+}
