9252cab3865e126a73b6e23e3017434777daf475
jordyvanderhaegen <jordy@esign.eu>
2025-12-03 09:21:46 +0100

    chore: provide failing test case

--- tests/OrchestralTestbenchCoreUsesVendorTest.php
+++ tests/OrchestralTestbenchCoreUsesVendorTest.php
@@ -1,15 +1,32 @@
 <?php
 
-
-use PHPUnit\Framework\Attributes\Test;
-
 namespace Esign\OrchestralTestbenchCoreUsesVendor\Tests;
 
+use Illuminate\Foundation\Auth\User;
+use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
+use Orchestra\Testbench\Attributes\UsesVendor;
+use Orchestra\Testbench\Attributes\WithMigration;
+use PHPUnit\Framework\Attributes\Test;
+
+#[WithMigration]
 final class OrchestralTestbenchCoreUsesVendorTest extends TestCase
 {
+    use LazilyRefreshDatabase;
+
     #[Test]
-    public function test(): void
+    public function it_can_interact_with_models(): void
     {
-        $this->assertTrue(true);
+        $this->expectNotToPerformAssertions();
+
+        User::query()->get();
+    }
+
+    #[Test]
+    #[UsesVendor]
+    public function it_can_interact_with_models_while_using_the_uses_vendor_attribute(): void
+    {
+        $this->expectNotToPerformAssertions();
+
+        User::query()->get();
     }
 }
